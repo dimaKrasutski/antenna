aa234debac80c3faeafad221c73d58fbd79d4404
dkrasutski <dmitri.krasutski@scorum.com>
2023-06-02 13:38:26 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -54,19 +54,25 @@ function App() {
           <div className="block-10" />
           <FormComponent />
 
-          <footer className="text-center mx-2">
+          <footer className="text-center">
           <span>
          <p className="mb-0">
-         ООО "рикстокмаркет",
-          </p> 
+         ООО "AМКонстал-Инжиниринг",
+         </p> 
+        <p className="mb-0">
+          220138, г. Минск ул. Липковская, д. 9/5, пом. 53
+        </p>
+        <p className="mb-0">
+            р/с BY26UNBS30121374800000001933 в ЗАО "БСБ Банк",  БИК UNBSBY2X г. Минск
+        </p>
+
+      
+             
+         
           <p className="mb-0">
-         УНП 193295196,
+         УНП 192958129,
           </p> 
             </span>
-            <p className="mb-0">
-            г. Минск ул. Тихая 6а
-            </p>
-
             <p className="mb-0 h6">Акция действует на всей территории Беларуси</p>
 
             <p className="mb-0">Телефон: +375 (29) 111-11-58</p>
