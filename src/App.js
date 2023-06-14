46c882f64b953bdff2e70c86680ef8c72118ed65
Johny Drama <johnydrama@Johnys-Laptop.local>
2023-06-14 13:06:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Antenna } from './components/pages/antenna/antenna';
-import { AirCondition } from './components/pages/air-condition/air-condition';
-import { Saw } from './components/pages/saw/saw';
-// import { Footer } from './components/footer/footer'; 
 import './App.css';
 
 // const Scr = () => <script src="//megatimer.ru/get/9f7b69e6a2125e5b35ca46f5e0c15b7f.js"></script>
@@ -18,8 +15,6 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="antenna-tv" element={<Antenna />} />
-            <Route path="air-condition" element={<AirCondition />} />
-            <Route path="saw" element={<Saw />} />
          </Routes>
         </BrowserRouter>
     )
