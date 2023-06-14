b21b876ff2adc99f7469486cd1b6e549e9c3617d
Johny Drama <johnydrama@Johnys-Laptop.local>
2023-06-14 13:23:08 +0300

    fix: add tt script tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Antenna } from './components/pages/antenna/antenna';
 import './App.css';
 
